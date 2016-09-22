255a7521bd8bebd1849ba46a53de5f0410863464
mmcilroy <m1rk.mci@gmail.com>
2016-09-22 20:27:54 +0100

    lazy connect to remote

--- src/proxy.cpp
+++ src/proxy.cpp
@@ -14,13 +14,13 @@ void hex_dump( void *addr, int len ) {
         }
         printf (" %02x", pc[i]);
         if( ( pc[i] < 0x20 ) || ( pc[i] > 0x7e ) ) {
-            buff[ i % 16 ] = '.';
+            buff[ i%16 ] = '.';
         } else {
-            buff[ i % 16 ] = pc[i];
+            buff[ i%16 ] = pc[i];
         }
-        buff[ (i % 16) + 1 ] = '\0';
+        buff[ ( i%16 ) + 1 ] = '\0';
     }
-    while( (i % 16) != 0 ) {
+    while( ( i%16 ) != 0 ) {
         printf( "   " ); i++;
     }
     printf( "  %s\n", buff );
@@ -33,8 +33,10 @@ public:
     proxy( const std::string& remote_host, short remote_port, short local_port ) :
         acceptor_( io_, tcp::endpoint( tcp::v4(), local_port ) ),
         remote_sock_( io_ ),
-        local_sock_( io_ ) {
-        connect_remote( remote_host, remote_port );
+        local_sock_( io_ ),
+        remote_host_( remote_host ),
+        remote_port_( remote_port ) {
+        accept_local();
     }
 
     void start() {
@@ -42,14 +44,14 @@ public:
     }
 
 private:
-    void connect_remote( const std::string& host, short port ) {
-        std::cout << "connecting to " << host << ":" << port << std::endl;
+    void connect_remote() {
+        std::cout << "connecting to " << remote_host_ << ":" << remote_port_ << std::endl;
         tcp::resolver resolver( io_ );
-        auto endpoint = resolver.resolve( { host, std::to_string( port ) } );
+        auto endpoint = resolver.resolve( { remote_host_, std::to_string( remote_port_ ) } );
         boost::asio::async_connect( remote_sock_, endpoint,
             [ this ]( boost::system::error_code ec, tcp::resolver::iterator ) {
                 if( !ec ) {
-                    accept_local();
+                    read_remote();
                 } else {
                     std::cout << "failed to connect to remote host" << std::endl;
                 }
@@ -58,12 +60,20 @@ private:
 
     void accept_local() {
         std::cout << "accept starting" << std::endl;
+        if( local_sock_.is_open() ) {
+            local_sock_.close();
+        }
+        if( remote_sock_.is_open() ) {
+            remote_sock_.close();
+        }
         acceptor_.async_accept( local_sock_,
             [ this ]( boost::system::error_code ec ) {
                 if( !ec ) {
                     std::cout << "accepted" << std::endl;
-                    read_remote();
+                    connect_remote();
                     read_local();
+                } else {
+                    std::cout << "accept failed: " << ec.message() << std::endl;
                 }
             } );
     }
@@ -72,10 +82,10 @@ private:
         remote_sock_.async_read_some( boost::asio::buffer( remote_buf_, max_length ),
             [ this ]( boost::system::error_code ec, std::size_t length ) {
                 std::cout << length << " bytes from remote" << std::endl;
-                if( length ) {
-                    hex_dump( remote_buf_, length );
-                }
                 if( !ec ) {
+                    if( length ) {
+                        hex_dump( remote_buf_, length );
+                    }
                     boost::asio::async_write( local_sock_, boost::asio::buffer( remote_buf_, length ),
                         [ this ]( boost::system::error_code ec, std::size_t length ) {
                             if( !ec ) {
@@ -94,10 +104,10 @@ private:
         local_sock_.async_read_some( boost::asio::buffer( local_buf_, max_length ),
             [ this ]( boost::system::error_code ec, std::size_t length ) {
                 std::cout << length << " bytes from local" << std::endl;
-                if( length ) {
-                    hex_dump( local_buf_, length );
-                }
                 if( !ec ) {
+                    if( length ) {
+                        hex_dump( local_buf_, length );
+                    }
                     boost::asio::async_write( remote_sock_, boost::asio::buffer( local_buf_, length ),
                         [ this ]( boost::system::error_code ec, std::size_t length ) {
                             if( !ec ) {
@@ -108,6 +118,7 @@ private:
                         } );
                 } else {
                     std::cout << "local connection closed" << std::endl;
+                    accept_local();
                 }
             } );
     }
@@ -116,6 +127,9 @@ private:
     tcp::acceptor acceptor_;
     tcp::socket remote_sock_;
     tcp::socket local_sock_;
+
+    std::string remote_host_;
+    short remote_port_;
 
     enum { max_length = 1024 };
     char remote_buf_[ max_length ];
